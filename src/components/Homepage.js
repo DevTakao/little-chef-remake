5ab22b0dcf63ca82be46a31a897839ede11e74c8
Takao21 <42912031+Takao21@users.noreply.github.com>
2020-09-22 22:00:21 +0630

    header sample created

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -6,6 +6,10 @@ function Homepage() {
     <div className="Homepage">
       <div className="hero-container">
         <div className="bg-screener"></div>
+        <div className="hero-captions text-right">
+          <h2 className="secondary-heading">Little Chef's</h2>
+          <h1 className="primary-heading">Award Winning Recipes</h1>
+        </div>
       </div>
     </div>
   );
