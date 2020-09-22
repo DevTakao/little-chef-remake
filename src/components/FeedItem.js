8356078622148cd364eb2b0ba3abe61b914474d6
Takao21 <42912031+Takao21@users.noreply.github.com>
2020-09-23 01:29:39 +0630

    neumorphism homepage OK

--- src/components/FeedItem.js
+++ src/components/FeedItem.js
@@ -10,6 +10,20 @@ function FeedItem() {
           alt="preview"
         />
       </div>
+      <div className="title-container">
+        <h5 className="text-center mt-2">Chicken Wings</h5>
+      </div>
+      <div className="content-container p-2">
+        <p>
+          Chicken wing, chicken wing, hotdog and macaroni, chilling with my
+          homies.
+        </p>
+      </div>
+      <div className="button-container">
+        <div className="neubutton">
+          <span>Read Recipe</span>
+        </div>
+      </div>
     </div>
   );
 }
