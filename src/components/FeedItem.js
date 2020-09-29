8e1bb058767b52dd04e725cb2b6dda53e750ef68
Takao21 <42912031+Takao21@users.noreply.github.com>
2020-09-29 19:02:10 +0630

    nav and fadein anim @details

--- src/components/FeedItem.js
+++ src/components/FeedItem.js
@@ -3,7 +3,6 @@ import "./FeedItem.css";
 import { Link } from "react-router-dom";
 import { RecipeContext } from "../App";
 
-
 function FeedItem({ displayData }) {
   const { recipeDetails, setRecipeDetails } = useContext(RecipeContext);
   return (
@@ -11,17 +10,21 @@ function FeedItem({ displayData }) {
       <div className="img-container">
         <img src={displayData.display.images[0]} alt="preview" />
       </div>
-      <div className="title-container m-2">
-        <h5 className="text-center mt-2">{displayData.display.displayName}</h5>
+      <div className="title-container mx-1 mt-3">
+        <h5 className="text-center">{displayData.display.displayName}</h5>
       </div>
-      <div className="content-container p-2">
+      <div className="content-container px-2">
         <p>{displayData.content.description.text}</p>
       </div>
       <div className="button-container">
-        <Link style={{textDecoration: "none"}} to={`/${displayData["tracking-id"]}`} onClick={() => setRecipeDetails(displayData.content)}>
-            <div className="neubutton">
-              <span>Read Recipe</span>
-            </div>
+        <Link
+          style={{ textDecoration: "none" }}
+          to={`/${displayData["tracking-id"]}`}
+          onClick={() => setRecipeDetails(displayData.content)}
+        >
+          <div className="neubutton">
+            <span>Read Recipe</span>
+          </div>
         </Link>
       </div>
     </div>
--- src/components/RecipeDetails.js
+++ src/components/RecipeDetails.js
@@ -1,5 +1,6 @@
 import Axios from "axios";
 import React, { useContext, useEffect } from "react";
+import { Link } from "react-router-dom";
 import { RecipeContext } from "../App";
 import Footer from "./Footer";
 import "./RecipeDetails.css";
@@ -36,6 +37,17 @@ function RecipeDetails({ match }) {
   return (
     recipeDetails.hasOwnProperty("description") && (
       <div className="RecipeDetails">
+        <nav className="navigation d-flex justify-content-between align-items-baseline">
+          <Link to="/" style={{ textDecoration: "none", color: "#000" }}>
+            <span>
+              <i className="far fa-arrow-alt-circle-left backBtn mr-2"></i>
+              {/* <strong>Home Page</strong> */}
+            </span>
+          </Link>
+          <span className="align-self-center nav-logo">
+            <h4>Little Chef's Recipes From Heaven</h4>
+          </span>
+        </nav>
         <div className="head-div container-fluid">
           <div className="row justify-content-around">
             <div className="col-12 col-lg-6">
@@ -45,17 +57,18 @@ function RecipeDetails({ match }) {
                   width: "100%",
                   height: "100%",
                   minHeight: "50vh",
+                  borderRadius: "2rem",
                   backgroundImage: `url(${recipeDetails["details"]["images"][0]["hostedLargeUrl"]})`,
                   backgroundSize: "contain",
                   backgroundRepeat: "no-repeat",
                   backgroundPosition: "center",
-                  backgroundColor: "#efefef",
+                  backgroundColor: "#fafafa",
                 }}
               ></div>
             </div>
             <div className="col-12 col-lg-6">
               <div className="head-text-div">
-                <h1>{recipeDetails.details.name}</h1>
+                <h1 className="details-title">{recipeDetails.details.name}</h1>
                 <hr></hr>
                 <p className="description-para">
                   {recipeDetails.description.text}
@@ -63,18 +76,22 @@ function RecipeDetails({ match }) {
                 <hr></hr>
                 <p>
                   <strong>
-                    <i className="fas fa-star"></i> Rating:{" "}
+                    <i className="fas fa-star"></i> Average Rating:{" "}
                   </strong>
                   {recipeDetails.details.rating} / 5
                 </p>
                 <hr></hr>
                 <p>
-                  <strong>Number of Servings:</strong>{" "}
+                  <strong>
+                    <i className="fas fa-utensils"></i> Number of Servings:
+                  </strong>{" "}
                   {recipeDetails.details.numberOfServings}
                 </p>
                 <hr></hr>
                 <p>
-                  <strong>Total Preparation Time:</strong>{" "}
+                  <strong>
+                    <i className="fas fa-clock"></i> Total Time:
+                  </strong>{" "}
                   {recipeDetails.details.totalTime}
                 </p>
                 <hr></hr>
@@ -92,9 +109,12 @@ function RecipeDetails({ match }) {
                   style={{ listStyleType: "circle" }}
                   className="ingredients-list"
                 >
-                  {recipeDetails.ingredientLines.map((ingredient, i) => (
-                    <li key={i}>{ingredient.wholeLine}</li>
-                  ))}
+                  {recipeDetails.ingredientLines.map(
+                    (ingredient, i) =>
+                      !!ingredient.wholeLine && (
+                        <li key={i}>{ingredient.wholeLine}</li>
+                      )
+                  )}
                 </ul>
               </div>
               <div className="col-6 d-flex align-items-center text-center">
@@ -112,11 +132,17 @@ function RecipeDetails({ match }) {
               <div className="col-6">
                 <h2>Preparation Steps</h2>
                 <hr></hr>
-                <ol className="ingredients-list">
+                <div className="ingredients-list">
                   {recipeDetails.preparationSteps.map((step, i) => (
-                    <li key={i}>{step}</li>
+                    <div className="prep-step" key={i}>
+                      <h6>
+                        <i className="fas fa-check-circle"></i>{" "}
+                        <strong>Step {i + 1}</strong>
+                      </h6>
+                      <p>{step}</p>
+                    </div>
                   ))}
-                </ol>
+                </div>
               </div>
             </div>
           </div>
